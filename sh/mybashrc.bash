f9638ee423b8c64dc1249e57adb62a0d221b3a29
Toby Elder <toby@ifixit.com>
2012-09-13 11:27:44 -0700

    Make all the filenames and linking correct.

--- sh/mybashrc.bash
+++ sh/mybashrc.bash
@@ -9,6 +9,10 @@ if [ -f ~/.alias ]; then
    . ~/.alias
 fi
 
+if [ -f ~/.git-completion.sh ]; then
+   . ~/.git-completion.sh
+fi
+
 
 export PS1="\[\033[36m\]\u\[\033[0m\]@\[\033[35m\]\H \[\033[32m\]\t \[\033[35m\]\W\[\033[0m\] \$ "
 export EDITOR=vim
